b3784d00ca7c321b33bffbc3b758b96c9338f0ee
neha-0415 <nehadandime1504@gmail.com>
2021-05-12 22:15:45 +0530

    My First Commit

--- app/src/main/java/com/example/kotlindemo/Main.kt
+++ app/src/main/java/com/example/kotlindemo/Main.kt
@@ -1,14 +1,5 @@
 package com.example.kotlindemo
 
 fun main(){
-    var a : String = "Android Kotlin"
-    var b : Float = 13.37F
-    val c : Double = 3.14159265358979
-    var d : Int = 25
-
-    println("$a")
-    println("$b")
-    println("$c")
-    println("$d")
+    print("Hello World!")
 }
-
