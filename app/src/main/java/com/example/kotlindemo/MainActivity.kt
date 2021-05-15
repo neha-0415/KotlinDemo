ae0859ad216c32196ffd7df2cafb57f56ff3ffcc
neha-0415 <nehadandime1504@gmail.com>
2021-05-15 21:12:33 +0530

    MyApp1

--- app/src/main/java/com/example/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.example.kotlindemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var btn = findViewById<Button>(R.id.btn3)
+        var txt = findViewById<TextView>(R.id.txt1)
 
+        var flag = false
+
+        btn.setOnClickListener(View.OnClickListener {
+            // Code here executes on main thread after user presses button
+
+            if(flag) {
+                txt.setText("Neha Dandime")
+                flag=false
+            }
+            else {
+                txt.setText("Hello World!!!")
+                flag=true
+            }
+        })
     }
 }
