7d356c1f94c13c0253c829f4b9cfab3e3114cc55
neha-0415 <nehadandime1504@gmail.com>
2021-05-23 22:25:31 +0530

    ChngeBackgrndColor App

--- app/src/main/java/com/example/myapp/MainActivity2.kt
+++ app/src/main/java/com/example/myapp/MainActivity2.kt
@@ -13,14 +13,13 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        var layout=findViewById<ConstraintLayout>(R.id.container)
-        var spinner=findViewById<Spinner>(R.id.spinner)
-        var button=findViewById<Button>(R.id.button)
+        var layout = findViewById<ConstraintLayout>(R.id.container)
+        var spinner = findViewById<Spinner>(R.id.spinner)
+        var button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener(View.OnClickListener {
-            var color:String=spinner.selectedItem.toString()
-            when(color)
-            {
+            var color: String = spinner.selectedItem.toString()
+            when (color) {
                 "Red" -> layout.setBackgroundColor(Color.RED)
                 "Green" -> layout.setBackgroundColor(Color.GREEN)
                 "Yellow" -> layout.setBackgroundColor(Color.YELLOW)
